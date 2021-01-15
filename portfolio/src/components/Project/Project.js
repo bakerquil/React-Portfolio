9eacd16aa64b2a8d2e890bc2c94e87b3a7845803
Baker Quillin <bakerquil@gmail.com>
2021-01-14 19:18:02 -0500

    heroku

--- portfolio/src/components/Project/Project.js
+++ portfolio/src/components/Project/Project.js
@@ -14,16 +14,17 @@ const Project = () => {
         github="https://github.com/v-lax/movie_trailer_web_app"
         />
          <Thumbnail
-        link="https://github.com/bakerquil/Budget_Tracker"
+        
         image="https://user-images.githubusercontent.com/63992745/103844317-a11fc700-5067-11eb-96a6-e36e91f8fd34.png"
         title="Budget Tracker"
         description="The budget tracker has had a service worker and a web manifest added so that the the application can not only have offline functionality to update when the user connects back to the web but also save the users data using mongodb."
+        github="https://github.com/bakerquil/Budget_Tracker"
         />
          <Thumbnail
-        link=""
-        image=""
-        title=""
-        description=""
+        image="https://user-images.githubusercontent.com/63992745/98742048-be902780-237b-11eb-9016-a2a44fa06976.png"
+        title="Day Planner"
+        description="A day planner than based off of the hour will make a field as current, past, or future. when you input the item into any field it will save it in local storage upon refresh."
+        github="https://github.com/bakerquil/Day_Planner"
         />
         
        </div>
--- portfolio/src/components/Thumbnail/Thumbnail.js
+++ portfolio/src/components/Thumbnail/Thumbnail.js
@@ -1,11 +1,11 @@
 import React from "react"
-
+import "./style.css"
 const Thumbnail = (props) => {
     return (
 <div className="project">
     
 <div className="project-image">
-    <img src={props.image} alt="Project"/>
+    <img src={props.image} alt="Project" className="proimg"/>
 </div>
 <div className="project-title">{props.title}</div>
 <div className="project-description">{props.description}</div>
